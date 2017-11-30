a2887c57135f86624ac21c93a028b5c1373e532d
Lukasz-Kolodzinski <kolodzinski.lukasz@gmail.com>
2017-11-30 19:21:03 +0100

    Subtraction

--- calculator.py
+++ calculator.py
@@ -5,4 +5,8 @@ def addition():
     b = float(input('Type second number '))
     print(a + b)
 
+def subtraction():
+    a = float(input('Type first number '))
+    b = float(input('Type first number '))
+
 
