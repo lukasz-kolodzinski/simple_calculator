69669456e9703622bb0df2f1ac0638534dc700f7
Lukasz-Kolodzinski <kolodzinski.lukasz@gmail.com>
2017-11-30 19:28:55 +0100

    Multiplication and division

--- calculator.py
+++ calculator.py
@@ -1,12 +1,21 @@
 #Simple calculator
 
 def addition():
-    a = float(input('Type first number '))
-    b = float(input('Type second number '))
+    a = int(input('Type first number '))
+    b = int(input('Type second number '))
     print(a + b)
 
 def subtraction():
-    a = float(input('Type first number '))
-    b = float(input('Type first number '))
+    a = int(input('Type first number '))
+    b = int(input('Type second number '))
+    print(a - b)
 
+def multiplication():
+    a = int(input('Type first number '))
+    b = int(input('Type second number '))
+    print(a * b)
 
+def division():
+    a = int(input('Type first number '))
+    b = int(input('Type second number '))
+    print(a / b)
